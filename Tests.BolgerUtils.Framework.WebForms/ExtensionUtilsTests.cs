30dd746b3f0b71b74d1ff6899ce3719c8aee817d
Matthew Bolger <xan1000@gmail.com>
2019-09-14 12:42:54 +1000

    Added fake tests.

--- Tests.BolgerUtils.Framework.WebForms/ExtensionUtilsTests.cs
+++ Tests.BolgerUtils.Framework.WebForms/ExtensionUtilsTests.cs
@@ -1,4 +1,9 @@
-﻿namespace Tests.BolgerUtils.Framework.WebForms
+﻿using System;
+using System.Web;
+using BolgerUtils.Framework.WebForms;
+using Xunit;
+
+namespace Tests.BolgerUtils.Framework.WebForms
 {
     public class ExtensionUtilsTests
     {
@@ -11,10 +16,25 @@
 
         #region HttpRequest
 
-        //public static string BeginUrl(this HttpRequest item)
-        //public static int? GetFormIntOrNull(this HttpRequest item, string name)
-        //public static string GetFormValue(this HttpRequest item, string name)
-        //public static string GetFormValueNotNull(this HttpRequest item, string name)
+        [Fact]
+        public void BeginUrlTest() =>
+            // Its pretty hard to test HttpRequest - this is a fake test.
+            Assert.Throws<NullReferenceException>(() => HttpContext.Current.Request.BeginUrl());
+
+        [Fact]
+        public void GetFormIntOrNullTest() =>
+            // Its pretty hard to test HttpRequest - this is a fake test.
+            Assert.Throws<NullReferenceException>(() => HttpContext.Current.Request.GetFormIntOrNull("ID"));
+
+        [Fact]
+        public void GetFormValueNotNullTest() =>
+            // Its pretty hard to test HttpRequest - this is a fake test.
+            Assert.Throws<NullReferenceException>(() => HttpContext.Current.Request.GetFormValueNotNull("ID"));
+
+        [Fact]
+        public void GetFormValueTest() =>
+            // Its pretty hard to test HttpRequest - this is a fake test.
+            Assert.Throws<NullReferenceException>(() => HttpContext.Current.Request.GetFormValue("ID"));
 
         #endregion
 
@@ -25,21 +45,28 @@
         #endregion
 
         #region Session
-        
-        //public static T Get<T>(this HttpSessionState item, string key)
-        //public static void Set<T>(this HttpSessionState item, string key, T value)
+
+        [Fact]
+        public void GetTest() =>
+            // Its pretty hard to test HttpSessionState - this is a fake test.
+            Assert.Throws<NullReferenceException>(() => HttpContext.Current.Session.Get<int>("ID"));
+
+        [Fact]
+        public void SetTest() =>
+            // Its pretty hard to test HttpSessionState - this is a fake test.
+            Assert.Throws<NullReferenceException>(() => HttpContext.Current.Session.Set("ID", 1));
 
         #endregion
 
         #region String
-        
+
         //public static string ConnectionString(this string item)
         //public static ConnectionStringSettings ConnectionStringSettings(this string item)
 
         #endregion
 
         #region TextBox
-        
+
         //public static int? ToIntOrNull(this TextBox item)
         //public static TimeSpan? ToTimeSpanOrNull(this TextBox item, string format = "h:mm tt")
 
