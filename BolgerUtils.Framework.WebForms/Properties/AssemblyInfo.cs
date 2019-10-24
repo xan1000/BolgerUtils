788292fbb2b9406f4b7b97985bdca3a36105fc70
Matthew Bolger <xan1000@gmail.com>
2019-10-24 22:49:54 +1100

    Package deployment.

--- BolgerUtils.Framework.WebForms/Properties/AssemblyInfo.cs
+++ BolgerUtils.Framework.WebForms/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("BolgerUtils.Framework.WebForms")]
-[assembly: AssemblyDescription("BolgerUtils.Framework.WebForms")]
+[assembly: AssemblyDescription("See https://bitbucket.org/xan1000/bolgerutils/src/master/BolgerUtils.Framework.WebForms/")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("BolgerUtils.Framework.WebForms")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.9.5.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
